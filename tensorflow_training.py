f15ed2929d414bf4f1bc496d2c633b83b0e58fed
Delaeter <joseph.delaeter@gmail.com>
2019-10-11 15:58:05 +0200

    import lib

---
+++ tensorflow_training.py
@@ -0,0 +1,14 @@
+# Matplotlib
+import matplotlib.pyplot as plt
+# Tensorflow
+import tensorflow as tf
+# Numpy and Pandas
+import numpy as np
+import pandas as pd
+# Ohter import
+import sys
+
+
+from sklearn.preprocessing import StandardScaler
+
+
